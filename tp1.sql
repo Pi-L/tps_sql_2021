8109bc37e5071b092101486ba083fbc2d45994d8
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-05-25 10:47:07 +0200

    [TP1][WIP] start

--- tp1.sql
+++ tp1.sql
@@ -1,1 +1,13 @@
-select * from city;
+
+-- 1
+-- PI name (world.country) SIGMA population > 100000000 (world.country)
+SELECT name FROM country WHERE population > 100000000;
+
+-- 2
+-- PI region (world.country)
+SELECT DISTINCT region as nomRegion FROM country; 
+
+-- 3
+--
+SELECT DISTINCT region as nomRegion FROM country
+WHERE continent = 'Europe';
