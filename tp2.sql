f658e956c33933094ece2568d878b44347b78dd1
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-05-25 16:04:06 +0200

    [TP2][1][WIP] -- 4 - EXCEPT WITH

--- tp2.sql
+++ tp2.sql
@@ -1,1 +1,12 @@
--- 1 - 1
+-- 1 - 1 -- South America
+SELECT ct.name FROM city AS ct
+JOIN country AS c
+	ON c.capital = ct.id
+WHERE c.continent = 'South America';
+
+-- 1 - 2 French
+SELECT c.name FROM country AS c
+JOIN countrylanguage AS cl
+	ON cl.countrycode = c.code
+WHERE cl.language = 'French' AND isofficial;
+
