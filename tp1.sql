40ce9691e33768f3f824c619f7ac2427ac799a7b
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-05-25 11:31:28 +0200

    [TP1][WIP] --5 -  slowly but surely -- failed to join for --5

--- tp1.sql
+++ tp1.sql
@@ -1,13 +1,39 @@
 
 -- 1
--- PI name (world.country) SIGMA population > 100000000 (world.country)
+-- RA -- Π name (world.country) σ population > 100000000 (world.country)
 SELECT name FROM country WHERE population > 100000000;
 
 -- 2
--- PI region (world.country)
+-- RA -- Π region (world.country)
 SELECT DISTINCT region as nomRegion FROM country; 
 
 -- 3
---
-SELECT DISTINCT region as nomRegion FROM country
-WHERE continent = 'Europe';
+-- RA -- Π region ρ(nomRegion/region) (world.country) σ continent = Europe (world.country)
+SELECT DISTINCT region as nomRegion 
+	FROM country 
+	WHERE continent = 'Europe';
+
+-- 4
+-- RA -- 
+SELECT * FROM country WHERE region = 'Southern Europe';
+
+-- 5
+-- RA -- 
+SELECT capital FROM country 
+	WHERE region = 'Western Europe';
+
+
+-- 5 bis -- my join didn't work :(
+-- RA --
+SELECT data_type as capitalType FROM information_schema.columns
+	WHERE table_name = 'country' AND column_name = 'capital';
+
+
+-- 6
+-- RA --
+SELECT DISTINCT language FROM countrylanguage WHERE isofficial ORDER BY language ASC;
+
+-- 7
+-- RA --
+
+
